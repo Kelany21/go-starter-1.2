0cacb8900dbe3c24ae9110f51d07fd0e931c8739
Kelany21 <32714650+Kelany21@users.noreply.github.com>
2019-12-29 16:01:24 +0200

    Add files via upload

---
+++ app/controllers/admin/settings/helpers.go
@@ -0,0 +1,80 @@
+package settings
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang-starter/app/models"
+	"golang-starter/app/requests/admin/setting"
+	"golang-starter/config"
+	"golang-starter/helpers"
+)
+
+/**
+* filter module with some columns
+ */
+func filter(g *gin.Context) []string {
+	var filter []string
+	if  g.Query("value") != ""{
+		filter = append(filter, `value like "%` + g.Query("value") + `%"`)
+	}
+	if  g.Query("name") != ""{
+		filter = append(filter, `name like "%` + g.Query("name") + `%"`)
+	}
+	if  g.Query("slug") != ""{
+		filter = append(filter, `slug like "%` + g.Query("slug") + `%"`)
+	}
+	if  g.Query("setting_type") != ""{
+		filter = append(filter, `setting_type like "%` + g.Query("setting_type") + `%"`)
+	}
+	return filter
+}
+
+/**
+* preload module with some preload conditions
+*/
+func preload() []string {
+	return []string{}
+}
+
+/**
+* here we will check if request valid or not
+ */
+func validateRequest(g *gin.Context) (bool , *models.Setting)   {
+	// init struct to validate request
+	row := new(models.Setting)
+	/**
+	* get request and parse it to validation
+	* if there any error will return with message
+	 */
+	err := setting.StoreUpdate(g.Request, row)
+	/***
+	* return response if there an error if true you
+	* this mean you have errors so we will return and bind data
+	 */
+	if helpers.ReturnNotValidRequest(err, g) {
+		return false , row
+	}
+	return true , row
+}
+
+/**
+* findOrFail Data
+ */
+func FindOrFail(id interface{}) (models.Setting , bool)  {
+	var oldRow models.Setting
+	config.DB.Where("id = ?" , id).Find(&oldRow)
+	if oldRow.ID != 0{
+		return   oldRow , true
+	}
+	return  oldRow , false
+}
+
+/**
+* update row make sure you used UpdateOnlyAllowColumns to update allow columns
+* use fill able method to only update what you need
+*/
+func updateColumns(row *models.Setting , oldRow models.Setting) models.Setting {
+	onlyAllowData := helpers.UpdateOnlyAllowColumns(row , models.SettingFillAbleColumn())
+	config.DB.Model(&oldRow).Updates(onlyAllowData)
+	newData  , _ :=  FindOrFail(oldRow.ID)
+	return newData
+}
---
+++ app/controllers/admin/settings/settings.go
@@ -0,0 +1,66 @@
+package settings
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang-starter/app/models"
+	"golang-starter/app/transformers"
+	"golang-starter/config"
+	"golang-starter/helpers"
+)
+
+/***
+* get all rows with pagination
+ */
+func Index(g *gin.Context) {
+	// array of rows
+	var rows []models.Setting
+	// query before any thing
+	paginator := helpers.Paging(&helpers.Param{
+		DB:      config.DB,
+		Page:    helpers.Page(g),
+		Limit:   helpers.Limit(g),
+		OrderBy: helpers.Order("id desc"),
+		Filters: filter(g),
+		Preload: preload(),
+		ShowSQL: true,
+	}, &rows)
+	// transform slice
+	paginator.Records = transformers.SettingsResponse(rows)
+	// return response
+	helpers.OkResponseWithPaging(g, helpers.DoneGetAllItems(g), paginator)
+}
+
+/***
+* return row with id
+ */
+func Show(g *gin.Context) {
+	// find this row or return 404
+	row, find := FindOrFail(g.Param("id"))
+	if !find {
+		helpers.ReturnNotFound(g, helpers.ItemNotFound(g))
+		return
+	}
+	// now return row data after transformers
+	helpers.OkResponse(g, helpers.DoneGetItem(g), transformers.SettingResponse(row))
+}
+
+/**
+* update category
+ */
+func Update(g *gin.Context) {
+	// check if request valid
+	valid, row := validateRequest(g)
+	if !valid {
+		return
+	}
+	// find this row or return 404
+	oldRow, find := FindOrFail(g.Param("id"))
+	if !find {
+		helpers.ReturnNotFound(g, helpers.ItemNotFound(g))
+		return
+	}
+	/// update allow columns
+	oldRow = updateColumns(row, oldRow)
+	// now return row data after transformers
+	helpers.OkResponse(g, helpers.DoneUpdate(g), transformers.SettingResponse(oldRow))
+}
