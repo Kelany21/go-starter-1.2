0437026f3b54d5ce0b8bd3b010fa638ff3d7b882
Kelany21 <32714650+Kelany21@users.noreply.github.com>
2019-12-29 15:52:54 +0200

    Create categories.go

---
+++ app/controllers/admin/categories/categories.go
@@ -0,0 +1,98 @@
+package categories
+
+import (
+	"golang-starter/app/models"
+	"golang-starter/app/transformers"
+	"golang-starter/config"
+	"golang-starter/helpers"
+
+	"github.com/gin-gonic/gin"
+)
+
+/***
+* get all rows with pagination
+ */
+func Index(g *gin.Context) {
+	// array of rows
+	var rows []models.Category
+	// query before any thing
+	paginator := helpers.Paging(&helpers.Param{
+		DB:      config.DB,
+		Page:    helpers.Page(g),
+		Limit:   helpers.Limit(g),
+		OrderBy: helpers.Order("id desc"),
+		Filters: filter(g),
+		Preload: preload(),
+		ShowSQL: true,
+	}, &rows)
+	// transform slice
+	paginator.Records = transformers.CategoriesResponse(rows)
+	// return response
+	helpers.OkResponseWithPaging(g, helpers.DoneGetAllItems(g), paginator)
+}
+
+/**
+* store new category
+ */
+func Store(g *gin.Context) {
+	// check if request valid
+	valid, row := validateRequest(g)
+	if !valid {
+		return
+	}
+	// create new row
+	config.DB.Create(&row)
+	//now return row data after transformers
+	helpers.OkResponse(g, helpers.DoneCreateItem(g), transformers.CategoryResponse(*row))
+}
+
+/***
+* return row with id
+ */
+func Show(g *gin.Context) {
+	// find this row or return 404
+	row, find := FindOrFail(g.Param("id"))
+	if !find {
+		helpers.ReturnNotFound(g, helpers.ItemNotFound(g))
+		return
+	}
+	// now return row data after transformers
+	helpers.OkResponse(g, helpers.DoneGetItem(g), transformers.CategoryResponse(row))
+}
+
+/***
+* delete row with id
+ */
+func Delete(g *gin.Context) {
+	// find this row or return 404
+	row, find := FindOrFail(g.Param("id"))
+	if !find {
+		helpers.ReturnNotFound(g, helpers.ItemNotFound(g))
+		return
+	}
+	DeleteRelated(row)
+	config.DB.Unscoped().Delete(&row)
+	// now return ok response
+	helpers.OkResponseWithOutData(g, helpers.DoneDelete(g))
+}
+
+/**
+* update category
+ */
+func Update(g *gin.Context) {
+	// check if request valid
+	valid, row := validateRequest(g)
+	if !valid {
+		return
+	}
+	// find this row or return 404
+	oldRow, find := FindOrFail(g.Param("id"))
+	if !find {
+		helpers.ReturnNotFound(g, helpers.ItemNotFound(g))
+		return
+	}
+	/// update allow columns
+	oldRow = updateColumns(row, oldRow)
+	// now return row data after transformers
+	helpers.OkResponse(g, helpers.DoneUpdate(g), transformers.CategoryResponse(oldRow))
+}
